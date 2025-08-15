fffd95d56bf9747c0a7be69524549831718e732e
wxfyes <70796015+wxfyes@users.noreply.github.com>
2025-08-15 21:28:38 +0800

    Update MainWindow.axaml.cs

--- v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
+++ v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
@@ -339,7 +339,7 @@ public partial class MainWindow : WindowBase<MainWindowViewModel>
 
     private void menuPromotion_Click(object? sender, RoutedEventArgs e)
     {
-        ProcUtils.ProcessStart($"{Utils.Base64Decode(Global.PromotionUrl)}?t={DateTime.Now.Ticks}");
+        ProcUtils.ProcessStart(Utils.Base64Decode(Global.PromotionUrl));
     }
 
     private void menuSettingsSetUWP_Click(object? sender, RoutedEventArgs e)
